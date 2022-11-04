6c99f1cf8d8bc43939462f73885994409fc175db
flo1166 <floriankorn93@web.de>
2022-11-04 16:54:28 +0100

    Logic of Knapsack
    Here are the heuristics of the knapsack. This class helps to pack a item into the packed field. Also it calculates if the weight can be packed (if there is capacity left).

---
+++ src/knapsackProblem/Knapsack.java
@@ -0,0 +1,38 @@
+package knapsackProblem;
+
+public class Knapsack {
+	
+	String[] packed;
+	double capacity;
+	
+	public Knapsack(double capacity) {
+		this.capacity = capacity;
+		this.packed = new String[0];
+	}
+	
+	public void pack(String name) {
+		int i;
+		
+		int lenPacked = packed.length + 1;
+		String[] newPacked = new String[lenPacked];
+		
+		if (lenPacked != 1) {
+			for (i = 0; i < (lenPacked - 1); i++) {
+				newPacked[i] = packed[i];
+			}
+		}
+		
+		newPacked[lenPacked - 1] = name;
+		this.packed = newPacked;	
+	}
+	
+	public boolean weightCheck(Item item) {
+		if (item.weight <= this.capacity) {
+			this.capacity -= item.weight;
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
